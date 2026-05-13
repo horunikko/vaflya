a9175a9eb6ff8e5831fd8cb23520ad614a3440cc
horunikko <egortega245@gmail.com>
2026-05-13 15:59:50 +0000

    Внёс клиент внутрь функций, изменил импорт конфига

--- app/service/nalog.py
+++ app/service/nalog.py
@@ -1,35 +1,38 @@
-import os
 import asyncio
 import logging
-from config import nalogo_config, pay_config
+import httpx
+
 from lib.nalogo import Client
 from lib.nalogo import UnauthorizedException
-from socksio.exceptions import ProtocolError
+
+from config import nalogo_config, payment_config
+
 
 logger = logging.getLogger(__name__)
 
 inn = nalogo_config.inn
 password = nalogo_config.password
 proxy = nalogo_config.proxy
-ONE_MONTH = pay_config.one_month
-THREE_MONTHS = pay_config.three_month
-ONE_YEAR = pay_config.one_year
+ONE_MONTH = payment_config.one_month
+THREE_MONTHS = payment_config.three_month
+ONE_YEAR = payment_config.one_year
 
 
 price_list = {"1": ONE_MONTH, "3": THREE_MONTHS, "12": ONE_YEAR}
 suffix = {"1": "", "3": "а", "12": "ев"}
 
-client = Client(
-    base_url="https://lknpd.nalog.ru/api",
-    storage_path="app/handlers/tokens.json",
-    device_id="my-device-123",
-    proxy=proxy
-)
-
 
 async def create_simple_receipt(month: int, user: str):
     """Формирует и отправляет чек в налоговую. Принимает количество месяцев подписки,
     ищет соответствующую цену, а также добавляет в чек TELEGRAM ID клиента"""
+
+    client = Client(
+        base_url="https://lknpd.nalog.ru/api",
+        storage_path="app/handlers/tokens.json",
+        device_id="my-device-123",
+        proxy=proxy
+    )
+
     if not await client.get_access_token():
         await client.create_new_access_token(inn, password)
 
@@ -47,7 +50,7 @@ async def create_simple_receipt(month: int, user: str):
             logger.error("Токен недействителен, получаем новый...")
             await client.create_new_access_token(inn, password)
 
-        except ProtocolError as e:
+        except (httpx.ProtocolError, httpx.ReadTimeout):
             logger.warning(f"Прокси упал, попытка достучаться до прокси {att}/3")
             await asyncio.sleep(2)
         
