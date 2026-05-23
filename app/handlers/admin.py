e332064b9898e93fa173bdce6aa815e6fd22024b
horunikko <horuniko@gmail.com>
2026-05-23 09:55:37 +0000

    .

--- app/handlers/admin.py
+++ app/handlers/admin.py
@@ -3,17 +3,21 @@ from aiogram import F
 from aiogram import Router
 from aiogram.types import CallbackQuery
 
-from service.remna_cmds import has_user_sub
+from service.remna_cmds import remna
+from service.nalog import create_simple_receipt
 
 router = Router()
 
 
 @router.callback_query(F.data == 'admin_menu')
 async def admin_menu(callback: CallbackQuery):
-    await callback.answer()
+    await callback.answer(cache_time=1)
 
     #kb = InlineKeyboardMarkup(inline_keyboard=[
     #    [InlineKeyboardButton(text='')]
+    #await create_simple_receipt(1, "test")
 
-    await callback.message.edit_caption(caption='<b>— — Админ панель — —</b>\n\n\n<i>Выберите действие кнопками ниже</i>',
-                                        parse_mode='HTML')
+    await callback.message.edit_caption(
+        caption='<b>— — Админ панель — —</b>\n\n\n<i>Выберите действие кнопками ниже</i>',
+        parse_mode='HTML'
+    )
